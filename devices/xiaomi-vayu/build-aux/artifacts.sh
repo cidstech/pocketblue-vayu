94ec9cf3ccdd5d98063b86abbfaebbc2d6794584
Cidstech <144923991+cidstech@users.noreply.github.com>
2026-02-16 06:57:22 -0800

    Update artifacts.sh

--- devices/xiaomi-vayu/build-aux/artifacts.sh
+++ devices/xiaomi-vayu/build-aux/artifacts.sh
@@ -1,1 +1,18 @@
+# placeholder
+#!/usr/bin/env bash
 
+set -uexo pipefail
+
+which 7z
+which git
+which python
+
+mv $OUT_PATH/uboot.zip ./
+7z x uboot.zip -o./uboot
+cp uboot/.output/u-boot.img $OUT_PATH/images/uboot.img
+
+git clone --depth=1 https://android.googlesource.com/platform/external/avb
+python avb/avbtool.py make_vbmeta_image --flags 2 --padding_size 4096 --output $OUT_PATH/images/vbmeta-disabled.img
+
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.sh $OUT_PATH/flash-xiaomi-nabu.sh
+install -Dm 0755 $DEVICE_PATH/flash-scripts/flash-xiaomi-nabu.cmd $OUT_PATH/flash-xiaomi-nabu.cmd
